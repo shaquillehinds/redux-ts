cd2c2866435ee158fd9ce74ea58413e6aa18ae5f
Shaquille <shaqdulove@hotmail.com>
2021-02-06 21:15:04 -0400

    first commit

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,28 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const initialState: RepositoriesState = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+const reducer = (state = initialState, action: Action): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+export default reducer;
